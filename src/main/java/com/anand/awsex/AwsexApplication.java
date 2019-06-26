3afaa2a333cad8ffd3154eabb83cf68e9ab2f6db
anand <22annd@gmail.com>
2019-06-26 21:21:00 +0530

    AWS cicd ex

---
+++ src/main/java/com/anand/awsex/AwsexApplication.java
@@ -0,0 +1,13 @@
+package com.anand.awsex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsexApplication.class, args);
+	}
+
+}
